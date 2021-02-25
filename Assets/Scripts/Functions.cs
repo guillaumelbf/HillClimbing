09b6365111ca732f8841c4b803cc49b8435d8b1e
LEFEBVRE Guillaume <gu.lefebvre@student.isartdigital.com>
2021-02-25 18:23:17 +0100

    create script

---
+++ Assets/Scripts/Functions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum FunctionsType
+{
+    SINUS
+}
+
+public class Functions
+{
+    private FunctionsType functionType;
+
+    public Functions(FunctionsType _functionsType)
+    {
+        functionType = _functionsType;
+    }
+}
---
+++ Assets/Scripts/TerrainGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGeneration : MonoBehaviour
+{
+
+    [SerializeField] private float maxXRange = 0;
+    [SerializeField] private float minXRange = 0;
+    [SerializeField] private float minYRange = 0;
+    [SerializeField] private float maxYRange = 0;
+
+    [SerializeField] private FunctionsType functionsType = FunctionsType.SINUS;
+
+    private Functions currentFunction = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentFunction = new Functions(functionsType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
